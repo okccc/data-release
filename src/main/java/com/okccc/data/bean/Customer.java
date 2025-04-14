db83cc9d3622204a8584cd3ecca4684590ade582
okccc <1573976179@qq.com>
2025-04-14 10:14:22 +0800

    TOO_MANY_REQUESTS/12/disk usage exceeded flood-stage watermark

--- src/main/java/com/okccc/data/bean/Customer.java
+++ src/main/java/com/okccc/data/bean/Customer.java
@@ -10,6 +10,10 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
  * @Author: okccc
  * @Date: 2024/3/27 15:27:24
  * @Desc:
+ *
+ * [TOO_MANY_REQUESTS/12/disk usage exceeded flood-stage watermark, index has read-only-allow-delete block];
+ * 原因：一次插入数据过多或短时间内请求次数过多,导致es节点内存超出限制,es会主动给索引上锁使其变成只读状态,需要关闭索引的只读状态
+ * 解决：PUT http://localhost:9200/_all/_settings {"index.blocks.read_only_allow_delete": null}
  */
 @Data
 @Document(indexName = "di")  // 将Bean和ES的Index进行映射
